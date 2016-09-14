de4d4ff8a343c8df3e60484508869d80c4fc6529
morm <tmozhar@gmail.com>
2016-09-14 15:08:50 +0300

    Update signature.js

--- src/signature.js
+++ src/signature.js
@@ -51,7 +51,7 @@ angular.module('signature').directive('signaturePad', ['$window', '$timeout',
              defer handling mouseup event until $scope.signaturePad handles
              first the same event
              */
-            $timeout()
+            $timeout(function(){})
               .then(function () {
                 var result = $scope.accept();
                 $scope.dataurl = result.isEmpty ? undefined : result.dataUrl;
@@ -74,37 +74,34 @@ angular.module('signature').directive('signaturePad', ['$window', '$timeout',
         canvas = element.find('canvas')[0];
         scope.signaturePad = new SignaturePad(canvas);
         
-        //Resize to fix UI Bootstrap Modal Show problem
-        $timeout(function(){
-            canvas.width = attrs.width;
-            canvas.height = attrs.height; 
-        }, 500);
-
-        if (scope.signature && !scope.signature.$isEmpty && scope.signature.dataUrl) {
-          scope.signaturePad.fromDataURL(scope.signature.dataUrl);
-        }
 
         scope.onResize = function() {
-          var canvas = element.find('canvas')[0];
-          var ratio =  Math.max($window.devicePixelRatio || 1, 1);
-          canvas.width = canvas.offsetWidth * ratio;
-          canvas.height = canvas.offsetHeight * ratio;
-          canvas.getContext("2d").scale(ratio, ratio);
+          var canvas = element.find('canvas')[0],
+          context = canvas.getContext('2d'),
+          devicePixelRatio  = window.devicePixelRatio || 1,
+          backingStoreRatio = context.webkitBackingStorePixelRatio ||
+                                 context.mozBackingStorePixelRatio ||
+                                 context.msBackingStorePixelRatio ||
+                                 context.oBackingStorePixelRatio ||
+                                 context.backingStorePixelRatio || 1,
+          ratio = devicePixelRatio / backingStoreRatio;
 
+          if (devicePixelRatio !== backingStoreRatio)
+          {          
+            var oldWidth = canvas.offsetWidth;
+            var oldHeight = canvas.offsetHeight;
+
+            canvas.width = oldWidth * ratio;
+            canvas.height = oldHeight * ratio;
+
+            canvas.style.width = oldWidth + 'px';
+            canvas.style.height = oldHeight + 'px';
+
+            context.scale(ratio, ratio);
+          }
           // reset dataurl
           scope.dataurl = null;
         }
-
-        scope.onResize();
-
-        angular.element($window).bind('resize', function() {
-            scope.onResize();
-        });
-
-        element.on('touchstart', onTouchstart);
-
-        element.on('touchend', onTouchend);
-
         function onTouchstart() {
           scope.$apply(function () {
             // notify that drawing has started
@@ -121,6 +118,31 @@ angular.module('signature').directive('signaturePad', ['$window', '$timeout',
             scope.notifyDrawing({ drawing: false });
           });
         }
+
+        //Resize to fix UI Bootstrap Modal Show problem
+        $timeout(function()
+        {
+          if(!!attrs.width && !!attrs.height)
+          {
+            canvas.width = attrs.width;
+            canvas.height = attrs.height; 
+          }
+        }, 500).then(function()
+        {
+          if (scope.signature && !scope.signature.$isEmpty && scope.signature.dataUrl) {
+            scope.signaturePad.fromDataURL(scope.signature.dataUrl);
+          }
+
+          scope.onResize();
+
+          angular.element($window).bind('resize', function() {
+              scope.onResize();
+          });
+
+          element.on('touchstart', onTouchstart);
+
+          element.on('touchend', onTouchend);
+        });
       }
     };
   }
